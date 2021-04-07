f1e5ac85f57af987f5b9d529713a5a9c4c7ac36a
Hatchet2k4 <hatchet2k4@gmail.com>
2021-04-06 21:00:47 -0400

    Started minimap, not functional yet

---
+++ MiniMap1.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+#onready var wallimg = preload("res://textures/miniwall.png")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#wallimg.draw(self, Vector2(10, 10))
+	pass
---
+++ MiniMap2.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var wallimg = preload("res://textures/miniwall.png")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	update()
+	
+func _draw():
+	wallimg.draw(self, Vector2(10, 10))
